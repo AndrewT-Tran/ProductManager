e3c56baf971ec3c2fd7d18ad92386d5b3ace97ef
Andrew T <me@andrewtran-shi.dev>
2023-07-15 08:21:09 -0700

    first init

---
+++ FullStackProject/server/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
+require("./config/mongoose.config"); // This is new
+app.use(cors());
+app.use(express.json()); // This is new
+app.use(express.urlencoded({ extended: true })); // This is new
+require("./routes/product.routes")(app);
+app.listen(port, () =>
+	console.log(`LITTT YOURE IN ! Listening on port: ${port}`)
+
+);
